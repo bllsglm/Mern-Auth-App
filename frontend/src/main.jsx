cdb670f9a9418e760867618646e703c49b0b04be
bllsglm <bilalsaglam92@gmail.com>
2023-11-26 20:45:59 +0300

    Add UI

---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {createBrowserRouter,createRoutesFromElements, Route ,RouterProvider} from "react-router-dom"
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+import HomeScreen from './Screens/HomeScreen.jsx'
+import LoginScreen from './Screens/LoginScreen.jsx'
+import RegisterScreen from './Screens/RegisterScreen.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <App /> }>
+      <Route path='/' index={true} element={<HomeScreen/>} />
+      <Route path='/login' element={<LoginScreen/>} />
+      <Route path='/register' element={<RegisterScreen/>} />
+    </Route>
+  ))
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} /> 
+  </React.StrictMode>,
+)
